45f3ef8813a3dfb10052f241ddae2726d9584f31
393560589@qq.com <393560589@qq.com>
2018-06-25 11:28:37 +0800

    theme.js中增加antd-mobile主题色的定制 每次改变都要重新运行npm start

---
+++ theme.js
@@ -0,0 +1,8 @@
+const primary='#E5021A';
+const primary_tap='#d30016';
+module.exports = {
+    primary_button_fill:primary,
+    primary_button_fill_tap:primary_tap,
+    ghost_button_color:primary,
+    ghost_button_fill_tap:primary_tap
+};
